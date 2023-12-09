ee9d2bd92b25e98b26cbd5650618fb4ef5f12224
Ducey101 <143230603+Ducey101@users.noreply.github.com>
2023-12-08 23:58:50 -0500

    Add files via upload

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,18 +1,16 @@
 // server/routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-const { body } = require('express-validator');
 const authController = require('../controllers/authController');
 const { connection } = require('../utils/db');
-const bcrypt = require('bcryptjs');
+const argon2 = require('argon2');
 
-router.post('/register', authController.register); // Temporarily bypass validation
+router.post('/register', authController.register); // Using the register function from authController
 
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     try {
-        // Query to find the user by username
         const query = 'SELECT * FROM users WHERE username = ?';
         connection.query(query, [username], async (err, results) => {
             if (err) {
@@ -20,27 +18,27 @@ router.post('/login', async (req, res) => {
             }
 
             if (results.length === 0) {
-                // User not found
-                return res.status(400).json({ message: 'Invalid username or password' });
+                return res.status(400).json({ message: 'Invalid username' });
             }
 
             const user = results[0];
-            const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+            console.log("Retrieved user data:", user); // For debugging
+
+            // Use the correct field name for the password hash
+            const passwordMatch = await argon2.verify(user.password_hash, password);
+            console.log(passwordMatch)
 
             if (!passwordMatch) {
-                // Passwords do not match
-                return res.status(400).json({ message: 'Invalid username or password' });
+                return res.status(400).json({ message: 'Invalid password' });
             }
 
             // Login successful
             res.json({ message: 'Login successful', user: { id: user.id, username: user.username } });
-            // Implement token generation or session creation as needed
         });
     } catch (error) {
-        console.error(error);
+        console.error("Login error:", error);
         res.status(500).json({ message: 'Server error' });
     }
 });
-
 
 module.exports = router;
