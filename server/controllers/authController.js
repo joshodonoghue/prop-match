48859996e7a986a8ae5647d9c7c807bc78e90e48
Ducey101 <143230603+Ducey101@users.noreply.github.com>
2023-12-08 23:56:05 -0500

    Add files via upload

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
 const { connection } = require('../utils/db'); // Ensure this path is correct
+const argon2 = require('argon2');
+
 
 exports.register = async (req, res) => {
     // Check for validation errors
@@ -13,10 +15,11 @@ exports.register = async (req, res) => {
         const { first_name, last_name, email, username, password } = req.body;
 
         // Log and check the received password
-        console.log("Received password:", password);
+        console.log("Received password:", req.body);
         if (!password) {
             return res.status(400).json({ message: "Password is required" });
         }
+        console.log("1");
 
         // Check if email or username already exists
         connection.query('SELECT * FROM users WHERE email = ? OR username = ?', [email, username], async (err, results) => {
@@ -29,10 +32,11 @@ exports.register = async (req, res) => {
             }
 
             // Hash password
-            const hashedPassword = await bcrypt.hash(password, 10);
-
+            const hashedPassword = await argon2.hash(password);
+            console.log("1:");
             // Create new user
             const query = 'INSERT INTO users (first_name, last_name, email, username, password_hash) VALUES (?, ?, ?, ?, ?)';
+            console.log(results);
             connection.query(query, [first_name, last_name, email, username, hashedPassword], (err, results) => {
                 if (err) {
                     return res.status(500).json({ message: err.message });
