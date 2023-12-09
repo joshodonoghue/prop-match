04efcda8c4cf71d72ca4d34137f09e05f5eda6e9
Ducey101 <143230603+Ducey101@users.noreply.github.com>
2023-12-08 23:58:26 -0500

    Add files via upload

--- server/models/User.js
+++ server/models/User.js
@@ -2,16 +2,19 @@
 const { connection } = require('../utils/db');
 
 const createUser = async (userData) => {
-    const { firstName, lastName, email, username, passwordHash } = userData;
-    const query = 'INSERT INTO users (firstName, lastName, email, username, passwordHash) VALUES (?, ?, ?, ?, ?)';
+    // Adjust these fields to match your database column names
+    const { first_name, last_name, email, username, password } = userData;
+    const query = 'INSERT INTO users (first_name, last_name, email, username, password) VALUES (?, ?, ?, ?, ?)';
 
     return new Promise((resolve, reject) => {
-        connection.query(query, [firstName, lastName, email, username, passwordHash], (err, results) => {
+        connection.query(query, [first_name, last_name, email, username, password], (err, results) => {
             if (err) {
                 reject(err);
             } else {
-                resolve(results.insertId); // returns the ID of the newly created user
+                resolve(results.insertId); // Returns the ID of the newly created user
             }
         });
     });
 };
+
+module.exports = { createUser };
